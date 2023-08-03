66f10bda51bb1dca27fdd323999192127e879d46
Hoskyllz <olabayoa009@gmail.com>
2023-08-03 11:37:38 +0100

    first commit

---
+++ controller/goalController.js
@@ -0,0 +1,66 @@
+const Goals = require('../models/goal')
+
+//get all goals
+const getAllGoals = async (req,res) => {
+    try {
+        const goals = await Goals.find()
+        res.status(200).json({success:true, goals})
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+
+//get a single goals
+const getGoal = async (req,res) => {
+    const { goalId } = req.params
+    try {
+        const goal = await Goals.findById({ _id: goalId })
+        res.status(200).json({success: true,goal})
+    } catch (error) {
+        res,json(error)
+    }
+}
+
+//create goal
+const createGoal = async (req, res) => {
+    const { title, description } = req.body 
+    if (!title || !description) {
+        return res.status(400).json({success: false, message: 'Please fill all the input fields'})
+    }
+   try {
+       const goal = await Goals.create({ ...req.body })
+       res.status(201).json({success: true,goal})
+   } catch (error) {
+    res.json(error)
+   }
+}
+
+//update goal
+const updateGoal = async (req,res) => {
+    const { goalId } = req.params
+    try {
+        const goal = await Goals.findByIdAndUpdate({ _id: goalId }, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        res.status(200).json({success: true, goal})
+    } catch (error) {
+        res.json (error)
+    }
+}
+
+//delete
+const deleteGoal = async (req, res) => {
+    const { goalId } = req.params
+    try {
+        const goal = await Goals.findByIdAndDelete({ _id: goalId })
+        res.status (200).json({success:true})
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+module.exports ={getAllGoals,getGoal,createGoal,updateGoal,deleteGoal}
+
+
---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 7000
+const mongoose = require("mongoose")
+const cors = require ('cors')
+const goalRouter = require("./routes/goalRouter")
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//route
+app.use("/api/goals", goalRouter)
+
+//db connection
+const start = async () => {
+   try {
+       mongoose.connect(process.env.MONGO_URI)
+       app.listen(PORT, () => {
+           console.log(`server running`);
+       })
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+
+start()
+
+
+//error
+app.use((req, res) => {
+    res.status(404).send ("Resource Not Found")
+})
+
